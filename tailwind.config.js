5001a4a876af9618b8c751a124fe0702d4b84628
marcoscrocci <marcoscrocci@gmail.com>
2022-04-09 19:16:09 -0300

    Login + Componentes: Button, InputText, Link

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx}",
+        "./src/components/**/*.{js,ts,jsx,tsx}",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: { backgroundColor: ["disabled"], cursor: ["disabled"] },
+    },
+    plugins: [],
+};

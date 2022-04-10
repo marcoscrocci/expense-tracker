cb2a31c72ee57ac70a091a7dabdcbe130bd34f4e
marcoscrocci <marcoscrocci@gmail.com>
2022-04-09 21:16:40 -0300

    Dashboard - Parte 1 - Melhorias no Button

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,27 +1,44 @@
-import React, { ButtonHTMLAttributes } from 'react'
+import React, { ButtonHTMLAttributes, ReactNode } from "react";
+import tw from "tailwind-styled-components";
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
-    children: React.ReactNode
+    children: ReactNode;
+    variant?: 'ghost'
 }
 
+const Button = ({ children, variant, ...props }: Props) => {
+    if (variant === 'ghost') return (
+        <GhostButton {...props}>
+            {children}
+        </GhostButton>
+    )
+    return (
+        <MainButton {...props}>
+            {children}
+        </MainButton>
+    );
+};
 
-const Button = ({ children, ...props }: Props) => {
-  return (
-    <button {...props}
-        className={`
-            bg-blue-700 
-            h-10 
-            text-white 
-            rounded 
-            hover:bg-blue-800 
-            cursor-pointer 
-            disabled:cursor-not-allowed 
-            disabled:bg-gray-200 
-        `}
-    >
-        {children}
-    </button>
-  )
-}
+export default Button;
 
-export default Button
+export const BaseButton = tw.button`
+    h-10
+    text-white
+    rounded
+    cursor-pointer
+    disabled:cursor-not-allowed
+    disabled:bg-gray-200
+    px-6
+    mx-2
+`;
+
+export const MainButton = tw(BaseButton)`
+    bg-blue-700
+    hover:bg-blue-800
+`;
+
+export const GhostButton = tw(BaseButton)`
+    bg-transparent
+    hover:bg-gray-200
+    text-blue-700
+`;
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import Button from "../../components/atoms/Button";
+import { Container } from "../../components/templates";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+    return (
+        <Container>
+            <header className={`
+                w-full
+                max-w-screen-lg
+                flex
+                justify-between
+                items-center
+                h-1/6 bg-red-100
+            `}>
+                <span className="font-bold text-2xl">Expanse Tracker</span>
+                <nav className="flex gap-4">
+                    <Button>Adicionar Nova Conta</Button>
+                    <Button variant='ghost'>Sair</Button>
+                </nav>    
+            </header>        
+        </Container>
+    );
+};
 
 export default Dashboard;
